66e6670c76eb78a203f2a25fd012c924d869a33d
hamed hamze <hamed.hamze2212@gmail.com>
2025-03-10 01:23:53 +0330

    Added navbar layout, h1 global style.

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div className="navbar bg-base-300 ">
+            <div className="flex-1 pl-5">
+                <Link href="/" className="mr-5">
+                    Logo
+                </Link>
+                <Link href="/users" className="btn btn-ghost normal-case text-xl">
+                    Users
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
