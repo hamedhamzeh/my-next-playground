5fb6e7c9a247eb8391bb0af5f42be34c7d7ed2cd
hamed hamze <hamed.hamze2212@gmail.com>
2025-03-15 11:10:12 +0330

    Getting and creating data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
+import { error } from "console";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: "hamed" },
-        { id: 2, name: "hanie" }
-    ]);
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
@@ -13,5 +14,20 @@ export async function POST(request: NextRequest) {
 
     const validation = schema.safeParse(body);
     if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
-    return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email
+        }
+    });
+
+    if (user) return NextResponse.json({ error: "User already exist" }, { status: 400 });
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    });
+    return NextResponse.json(newUser, { status: 201 });
 }
