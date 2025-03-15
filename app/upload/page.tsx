6246cd428b25dd2318b99894e42968816099f588
hamed hamze <hamed.hamze2212@gmail.com>
2025-03-15 14:19:03 +0330

    Showing uploaded image.

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+    public_id: string;
+}
 
 const UploadPage = () => {
+    const [publicId, setPublicId] = useState("");
+
     return (
-        <CldUploadWidget uploadPreset="cpj6ywds" onUpload={(result, widget) => console.log(result)}>
-            {({ open }) => (
-                <button className="btn btn-primary" onClick={() => open()}>
-                    Upload
-                </button>
-            )}
-        </CldUploadWidget>
+        <>
+            {publicId && <CldImage src={publicId} width={270} height={180} alt="uploaded image" />}
+            <CldUploadWidget
+                uploadPreset="cpj6ywds"
+                onSuccess={(result) => {
+                    const info = result.info as CloudinaryResult;
+                    console.log("Uploaded Image Public ID:", info.public_id);
+                    setPublicId(info.public_id);
+                }}
+            >
+                {({ open }) => (
+                    <button className="btn btn-primary" onClick={() => open()}>
+                        Upload
+                    </button>
+                )}
+            </CldUploadWidget>
+        </>
     );
 };
 
